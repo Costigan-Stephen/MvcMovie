266c495ae2c9a53e95e1790fe08910eb5d1c2f7e
Steve <stephenjcostigan@hotmail.com>
2020-03-07 21:07:37 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IActionResult OnGetPartial() => 
+            new PartialViewResult
+        {
+            ViewName = "carousel",
+            ViewData = ViewData,
+        };
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return Redirect("../Movies/Create");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "17 Miracles",
+                        ReleaseDate = DateTime.Parse("2011-6-3"),
+                        Genre = "Adventure",
+                        Rating = "PG-13",
+                        Price = 7.99M,
+                        Image = "assets/covers/miracles.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Joseph Smith: The Prophet of the Restoration",
+                        ReleaseDate = DateTime.Parse("2005-12-24"),
+                        Genre = "Drama",
+                        Rating = "NR",
+                        Price = 8.99M,
+                        Image = "assets/covers/"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Charly",
+                        ReleaseDate = DateTime.Parse("2002-9-20"),
+                        Genre = "Drama",
+                        Rating = "PG-13",
+                        Price = 9.99M,
+                        Image = "assets/covers/charly.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "The other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2002-4-12"),
+                        Genre = "Adventure",
+                        Rating = "PG-13",
+                        Price = 9.99M,
+                        Image = "assets/covers/heaven.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Saints and Soldiers",
+                        ReleaseDate = DateTime.Parse("2005-3-25"),
+                        Genre = "Action",
+                        Rating = "PG-13",
+                        Price = 7.99M,
+                        Image = "assets/covers/soldiers.jpg"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
